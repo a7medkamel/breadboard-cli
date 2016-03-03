53ea7d55fbf018d4ef0e804d7d054b5485b3ef16
Ahmed Kamel <ak@ahmedkamel.net>
2016-03-03 03:16:08 -0800

    allow to run from cli / doesn't accept pipe yet

--- index.js
+++ index.js
@@ -1,10 +1,32 @@
 var search  = require('./lib/search')
   , run     = require('./lib/run')
+  , argv    = require('minimist')(process.argv.slice(2));
   ;
 
 if (!module.parent) {
-    // todo [akamel] support cli stream stdin
-    run();
+  var url = argv['_'][0];
+  if (!url) {
+    throw new Error('script url is required');
+  }
+
+  // http://stackoverflow.com/questions/15466383/how-to-detect-if-a-node-js-script-is-running-through-a-shell-pipe
+  var input = process.stdin.isTTY? argv['i'] : process.stdin;
+
+  run(url, input)
+    .then(function(result){
+      if (process.stdin.isTTY && result.encoding === 'binary') {
+        return result.open();
+      }
+      
+      return result
+                .createReadStream()
+                .then(function(stream){
+                  stream.pipe(process.stdout);
+                });
+    })
+    .catch(function(err){
+      console.error(err);
+    });
 }
 
 module.exports = {
