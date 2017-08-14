3d13cc47944f019ad37322e8fb6d127c01d5f19f
Ahmed Kamel <ahmed.kamel@docusign.com>
2017-08-14 16:17:58 -0700

    global module refactor

--- index.js
+++ index.js
@@ -1,17 +1,2 @@
-var program = require('commander')
-  , cmd_run = require('lib/cmd/run')
-  ;
 
-program
-  .version('0.1.0')
-  .arguments('<cmd> [owner] [repo]')
-  .option('-h, --host [value]', 'breadboard host')
-  .option('-g, --git [value]', 'git host')
-  .option('-f, --filename [value]', 'local file to run')
-  .option('-t, --token [value]', 'token')
-  .action((cmd, owner, repo) => {
-    let { filename, host, git, token } = program;
-
-    return cmd_run.exec(cmd, { owner, repo, host, git, filename, token });
-  })
-  .parse(process.argv);
+require('./lib/program').exec();
